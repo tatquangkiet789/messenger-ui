7cbb651a4106d17c77172a09d6134ee5e104a07c
Tất Quảng Kiệt <52742909+tatquangkiet789@users.noreply.github.com>
2023-10-06 18:42:05 +0700

    Đã thu hồi quyền truy cập camera và audio khi close call

--- src/pages/VideoCallPage/VideoCallPage.tsx
+++ src/pages/VideoCallPage/VideoCallPage.tsx
@@ -1,7 +1,9 @@
+import { ROUTES } from '@src/constants/routes';
 import SOCKET_EVENT from '@src/constants/socket';
 import { useAppSelector } from '@src/hooks/useAppSelector';
 import socketClient from '@src/lib/socketClient';
 import { FC, Fragment, useEffect, useRef, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import SimplePeer from 'simple-peer';
 
 type Call = {
@@ -15,6 +17,10 @@ const VideoCallPage: FC = () => {
     const [stream, setStream] = useState<MediaStream>();
     const myVideoRef = useRef<HTMLVideoElement | null>(null);
     const userVideoRef = useRef<HTMLVideoElement | null>(null);
+
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = (location.state as any)?.from.pathname || ROUTES.HOME;
 
     const { currentUser } = useAppSelector((state) => state.auth);
     const { receiver } = useAppSelector((state) => state.friends);
@@ -34,20 +40,23 @@ const VideoCallPage: FC = () => {
         navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
             setStream(stream);
             if (myVideoRef.current) myVideoRef.current.srcObject = stream;
+            if (userVideoRef.current) userVideoRef.current.srcObject = stream;
         });
 
         socketClient.emit(SOCKET_EVENT.SEND_VIDEO_CALL_RECEIVER_ID, { userId: receiver.id });
-        socketClient.on(
-            SOCKET_EVENT.RECEIVE_VIDEO_CALL_RECEIVER_ID,
-            ({ videoCallReceiverSocketId }: { videoCallReceiverSocketId: string }) => {
-                setVideoReceiverId(videoCallReceiverSocketId);
-            },
-        );
+        // socketClient.on(
+        //     SOCKET_EVENT.RECEIVE_VIDEO_CALL_RECEIVER_ID,
+        //     ({ videoCallReceiverSocketId }: { videoCallReceiverSocketId: string }) => {
+        //         console.log(`socketClient.on(SOCKET_EVENT.RECEIVE_VIDEO_CALL_RECEIVER_ID)`);
+        //         setVideoReceiverId(videoCallReceiverSocketId);
+        //     },
+        // );
 
-        socketClient.on('call-user', (data: any) => {
-            const { signal, from, name } = data;
-            setCallDetail({ isReceivedCall: true, callFrom: from, callerName: name, signal });
-        });
+        // socketClient.on(SOCKET_EVENT.CALL_USER, (data: any) => {
+        //     console.log(`socketClient.on(SOCKET_EVENT.CALL_USER)`);
+        //     const { signal, from, name } = data;
+        //     setCallDetail({ isReceivedCall: true, callFrom: from, callerName: name, signal });
+        // });
     }, []);
 
     // navigator.permissions.query({ name: microphonePermission }).then((permisson) => {
@@ -57,39 +66,51 @@ const VideoCallPage: FC = () => {
     //     console.log(permisson);
     // });
 
-    const answerCall = () => {
-        setIsAccepted(true);
-        const peer = new SimplePeer({ initiator: false, trickle: false, stream });
-        peer.on('signal', (data: any) => {
-            socketClient.emit(SOCKET_EVENT.ANSWER_CALL, { signal: data, to: call.from });
-        });
-        peer.on('stream', (stream: any) => {
-            if (userVideoRef.current) userVideoRef.current.srcObject = stream;
-        });
-        if (callDetail) peer.signal(callDetail.signal);
-        connectionRef.current = peer;
-    };
+    // const answerCall = () => {
+    //     setIsAccepted(true);
+    //     const peer = new SimplePeer({ initiator: false, trickle: false, stream });
+    //     peer.on('signal', (data: any) => {
+    //         socketClient.emit(SOCKET_EVENT.ANSWER_CALL, { signal: data, to: callDetail!.from });
+    //     });
+    //     peer.on('stream', (stream: any) => {
+    //         if (userVideoRef.current) userVideoRef.current.srcObject = stream;
+    //     });
+    //     if (callDetail) peer.signal(callDetail.signal);
+    //     connectionRef.current = peer;
+    // };
 
-    const callUser = (id: any) => {
-        const peer = new SimplePeer({ initiator: true, trickle: false, stream });
-        peer.on('signal', (data: any) => {
-            socketClient.emit('call-user', { userToCall: id, signalData: data, from: me, name });
-        });
-        peer.on('stream', (currentStream: any) => {
-            if (userVideoRef.current) userVideoRef.current.srcObject = currentStream;
-        });
-        socketClient.on('call-accepted', (signal: any) => {
-            setIsAccepted(true);
+    // const callUser = (id: any) => {
+    //     const peer = new SimplePeer({ initiator: true, trickle: false, stream });
+    //     peer.on('signal', (data: any) => {
+    //         socketClient.emit('call-user', { userToCall: id, signalData: data, from: me, name });
+    //     });
+    //     peer.on('stream', (currentStream: any) => {
+    //         if (userVideoRef.current) userVideoRef.current.srcObject = currentStream;
+    //     });
+    //     socketClient.on('call-accepted', (signal: any) => {
+    //         setIsAccepted(true);
 
-            peer.signal(signal);
-        });
-        connectionRef.current = peer;
-    };
+    //         peer.signal(signal);
+    //     });
+    //     connectionRef.current = peer;
+    // };
 
     // const leaveCall = () => {
     //     setIsEnded(true);
     //     if (connectionRef.current) connectionRef.current?.destroy();
     // };
+
+    const handleStop = () => {
+        if (!stream) return;
+
+        stream.getVideoTracks()[0].stop();
+        stream.getAudioTracks()[0].stop();
+
+        if (myVideoRef.current) myVideoRef.current.srcObject = null;
+        if (userVideoRef.current) userVideoRef.current.srcObject = null;
+
+        return navigate(from);
+    };
 
     return (
         <Fragment>
@@ -107,18 +128,23 @@ const VideoCallPage: FC = () => {
                     ) : (
                         <CallPeding />
                     )} */}
-                    <h1>My Video</h1>
-                    <div className='w-1/2'>
-                        <video className={`w-full h-full`} ref={myVideoRef} autoPlay muted></video>
+                    <div className={`flex flex-col justify-center items-center`}>
+                        <h1>My Video</h1>
+                        <video className={`w-1/2`} ref={myVideoRef} autoPlay muted></video>
                     </div>
+                    <div className={`flex flex-col justify-center items-center`}>
+                        <h1>User Video</h1>
+                        <video className={`w-1/2`} ref={userVideoRef} autoPlay muted></video>
+                    </div>
+                    <button onClick={handleStop}>Stop</button>
                 </div>
             ) : null}
 
-            <input type='text' value={idToCall} onChange={(e) => setIdToCall(e.target.value)} />
+            {/* <input type='text' value={idToCall} onChange={(e) => setIdToCall(e.target.value)} />
             <button onClick={() => callUser(idToCall)}>Call</button>
-            <button onClick={() => console.log(me)}>Copy ID</button>
+            <button onClick={() => console.log(me)}>Copy ID</button> */}
 
-            {isAccepted ? (
+            {/* {isAccepted ? (
                 <div
                     className='w-screen h-screen bg-gray241_241_242_1 flex flex-col 
                     justify-end items-center'
@@ -128,17 +154,17 @@ const VideoCallPage: FC = () => {
                         <video className={`w-full h-full`} ref={userVideoRef} autoPlay></video>
                     </div>
                 </div>
-            ) : null}
+            ) : null} */}
 
-            {call.isReceivedCall && !isAccepted ? (
+            {/* {callDetail!.isReceivedCall && !isAccepted ? (
                 <div
                     className='flex items-center justify-center gap-5 absolute 
                             bottom-5 left-0 right-0 mx-auto'
                 >
-                    {/* <CallAction stream={stream} /> */}
+                    <CallAction stream={stream} />
                     <button onClick={answerCall}>Answer call</button>
                 </div>
-            ) : null}
+            ) : null} */}
         </Fragment>
     );
 };

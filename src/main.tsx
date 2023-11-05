423c7890fcf910b787cb312e5e9e6fe0b98aa864
Tất Quảng Kiệt <52742909+tatquangkiet789@users.noreply.github.com>
2023-11-05 20:26:36 +0700

    Thêm react-query dependency

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,20 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './store.ts';
 import VideoProvider from './features/videos/context/VideoContext.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Provider store={store}>
-            <VideoProvider>
-                <App />
-            </VideoProvider>
-        </Provider>
+        <QueryClientProvider client={queryClient}>
+            <Provider store={store}>
+                <VideoProvider>
+                    <App />
+                </VideoProvider>
+            </Provider>
+            <ReactQueryDevtools />
+        </QueryClientProvider>
     </React.StrictMode>,
 );
